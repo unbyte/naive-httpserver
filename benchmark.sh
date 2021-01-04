d56b5e144e1589bae782261639c9bf573f0617c7
Helios <i@shangyes.net>
2021-01-04 18:00:56 +0800

    test: update benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -1,18 +1,29 @@
+echo -e "\nUse Wrk\n"
+
+wrk -t10 -c200 -d10s http://127.0.0.1:4399/ > wrk.bench
+cat common.bench 1>&2
+
+echo -e "\nDone.\n"
+
+sleep 5
+
+echo -e "\nUse ab\n"
+
 echo -e "\nCommon scene\n"
 ab -c 50 -n 1500 http://127.0.0.1:4399/ > common.bench
 cat common.bench 1>&2
 
-sleep 12
+sleep 5
 
 echo -e "\nConnection: keep-alive\n"
 ab -k -c 100 -n 50000 http://127.0.0.1:4399/ > keep-alive.bench
 cat keep-alive.bench 1>&2
 
-sleep 32
+sleep 5
 
 echo -e "\nPost with body\n"
 dd if=/dev/urandom of=test_body bs=1024 count=1 2> /dev/null
-ab -c 20 -n 1000 -p test_body http://127.0.0.1:4399/echo > post-body.bench
+ab -c 5 -n 200 -p test_body http://127.0.0.1:4399/echo > post-body.bench
 cat post-body.bench 1>&2
 
 echo -e "\nClear temp files\n"
